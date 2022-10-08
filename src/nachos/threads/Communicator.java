c5ba52d0fdae3e4225f70b4bab1a4934f87d34fd
Samuel Chih <30818360+SamuelChih@users.noreply.github.com>
2022-10-07 20:17:53 -0700

    Done Com and alarm

--- src/nachos/threads/Communicator.java
+++ src/nachos/threads/Communicator.java
@@ -1,6 +1,5 @@
 package nachos.threads;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 import nachos.machine.*;
@@ -40,7 +39,8 @@ public class Communicator {
         lock =new Lock();
 		speakers=new Condition2(lock);
 		listeners=new Condition2(lock);
-        listenmsg=0;
+        speakerQueue = new LinkedList<Message>();
+        listenerQueue = new LinkedList<Message>();
     }
 
     /**
@@ -57,15 +57,17 @@ public class Communicator {
      */
     public void speak(int word) {
         lock.acquire();
-        while (listenmsg == 1){
+        if(!listenerQueue.isEmpty()){
+            //remove the first listenerQueue and get the first number
+            Message listenMessage = listenerQueue.removeFirst();
+            listenMessage.msg = word;
+            listeners.wake();   
+        }
+        else{
+            //add the word to the speakerQueue
+            speakerQueue.add(new Message(word));
             speakers.sleep();
         }
-        currentmsg = word;
-        ++listenmsg;
-        if (listenmsg == 1){
-            listeners.wake();
-        } 
-
         lock.release();
     }
 
@@ -76,25 +78,40 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
+        int msg =0;
         lock.acquire();
-        while (listenmsg==0) {
+        if(!speakerQueue.isEmpty()){
+            //remove the first listenerQueue and get the first number
+            Message speakMessage =speakerQueue.removeFirst();
+            msg=speakMessage.msg;
+            speakers.wake(); 
+        }
+        else{
+            Message newMessage = new Message();
+            listenerQueue.add(newMessage);
             listeners.sleep();
+            msg = newMessage.msg;
         }
-        --listenmsg;
-        if (listenmsg==0) {
-            speakers.wake();
-        }
-
+        
         lock.release();
-        return currentmsg;
+        return msg;
+    }
+    
+
+    private class Message {
+        
+        private int msg;
+        private Message(){
+            msg = 0;
+        }
+        private Message(int i){
+            this.msg=i;
+        }
     }
 
-    private int currentmsg;
     private Lock lock;
     private Condition2 speakers;
     private Condition2 listeners;
-    private int listenmsg;
-    
-    private ArrayList <Integer> speakerMsg  = new ArrayList<Integer>();
-    private ArrayList <Integer> listenerMsg  = new ArrayList<Integer>();
+    private LinkedList <Message> speakerQueue;
+    private LinkedList <Message> listenerQueue;
 }
